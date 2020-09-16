2b4a3ebd2a8ac7b787fca4a64d40a5715307860f
prakashbishwas <71381358+prakashbishwas@users.noreply.github.com>
2020-09-16 16:02:51 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+public class EvenOdd{
+
+     public static void main(String []args){
+        int n=85;
+        
+        if(n%2==0)
+        {
+            System.out.println(" Even");
+        }
+        if(n%2!=0)
+        {
+        System.out.println("Odd");
+        }
+     }
+}
